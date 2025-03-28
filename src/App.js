8fe74b324a80d33f88543eef5c67212c81390496
yogeshv05 <yogesh.v2023@vitstudent.ac.in>
2025-03-28 08:46:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import React from "react";
-import Q11 from "./lab13q1.js";
-import Q2 from "./lab13q2.js";
-import Q3 from "./lab13q3.js";
-import Q4 from "./lab13q4.js";
-import Q5 from "./lab13q5.js";
-import Q6 from "./lab13q6.js";
-import Q7 from "./lab13q7.js";
-import Q8 from "./lab13q8.js";
-import Q9 from "./lab13q9.js";
-import Q10 from "./lab13q10.js";
-import Q111 from "./lab13q11.js";
+const files = [
+  'lab13q1', 'lab13q2', 'lab13q4', 'lab13q5', 
+  'lab13q6', 'lab13q7', 'lab13q8', 'lab13q9', 'lab13q10', 
+  'lab13q11', 'lab13q12', 'lab14q1', 'lab14q2', 'lab14q3'
+];
+
+const loadComponent = (file) => {
+  return lazy(() => import(`./${file === 'lab14q3' ? 'index' : file}.js`));
+};
+
+function DynamicTable() {
+  return (
+    <div>
+      <h1 align="center">My React Exercises</h1>
+      <table border="2" cellSpacing={0} cellPadding={5} align="center">
+        <thead>
+          <tr>
+            <th>Exercise Name</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map((file) => (
+            <tr key={file}>
+              <td>{file}</td>
+              <td>
+                <Link to={`/${file}`}>Open</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-        <Q11 />
-    </div>
-);
+    <Router basename="/web-programming-reactjs">
+      <Routes>
+        <Route path="/" element={<DynamicTable />} />
+        {files.map((file) => {
+          const Component = loadComponent(file);
+          return (
+            <Route
+              key={file}
+              path={`/${file}`}
+              element={
+                <Suspense fallback={<h2>Loading...</h2>}>
+                  <Component />
+                </Suspense>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
